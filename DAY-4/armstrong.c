d6d1c478405d0849c5eab12864a90838c2aba080
PUSHKAR-SINGH-BHARADWAJ <pushkarsinghbharadwaj@gmail.com>
2026-06-04 12:06:02 +0530

    DAY-4 assignment is uploaded

---
+++ DAY-4/armstrong.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n, temp, rem, sum = 0;
+printf("enter the value of n");
+scanf("%d", &n);
+    temp = n;
+ while (temp > 0) {
+        rem = temp % 10;
+        sum += rem * rem * rem;
+        temp /= 10;
+    }if (sum == n)
+        printf("Armstrong Number");
+    else
+        printf("Not Armstrong Number");
+    return 0;
+}
